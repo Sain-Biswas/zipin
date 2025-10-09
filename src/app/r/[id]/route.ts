1c6529f74eadd2cfe0dc124194dacc3085425a78
Sain-Biswas <sain.biswas614@gmail.com>
2025-10-10 03:51:09 +0530

    feat: add redirect route handler

--- src/app/r/[id]/route.ts
+++ src/app/r/[id]/route.ts
@@ -1,6 +1,9 @@
 import { geolocation } from "@vercel/functions";
+import { eq } from "drizzle-orm";
 import { type NextRequest, NextResponse } from "next/server";
 import { getCountryName } from "~/lib/get-country-name";
+import { database } from "~/server/database/index.database";
+import { clicksSchema, urlSchema } from "~/server/database/index.schema";
 
 export async function GET(
   req: NextRequest,
@@ -10,14 +13,27 @@ export async function GET(
     const param = await params;
 
     const location = geolocation(req);
+    const origin = getCountryName(location.country);
 
-    return NextResponse.json(
-      {
-        id: param.id,
-        random: getCountryName(location.country)
-      },
-      { status: 200 }
-    );
+    const url = await database.query.urlSchema.findFirst({
+      where: eq(urlSchema.id, param.id)
+    });
+
+    if (!url)
+      return NextResponse.json(
+        {
+          success: false,
+          message: "The requested url either doesn't exist or is expired."
+        },
+        { status: 404 }
+      );
+
+    await database.insert(clicksSchema).values({
+      urlId: url.id,
+      origin
+    });
+
+    return NextResponse.redirect(url.originalUrl);
   } catch (error: unknown) {
     console.log(error);
 
