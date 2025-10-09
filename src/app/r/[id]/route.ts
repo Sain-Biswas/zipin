2e9b19bd1a563cb032850c0e4d749f7f7e2ef83c
Sain-Biswas <sain.biswas614@gmail.com>
2025-10-10 01:37:07 +0530

    feat: add geolocation api

--- src/app/r/[id]/route.ts
+++ src/app/r/[id]/route.ts
@@ -1,16 +1,31 @@
-import { NextResponse, type NextRequest } from "next/server";
+import { geolocation } from "@vercel/functions";
+import { type NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   req: NextRequest,
   { params }: RouteContext<"/r/[id]">
 ) {
-  const param = await params;
+  try {
+    const param = await params;
 
-  return NextResponse.json({
-    id: param.id,
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    location: req.geo
-  });
+    const location = geolocation(req);
+
+    return NextResponse.json(
+      {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        id: param.id,
+        location
+      },
+      { status: 200 }
+    );
+  } catch (error: unknown) {
+    console.log(error);
+
+    return NextResponse.json(
+      {
+        massage: "Internal Server Error"
+      },
+      { status: 500 }
+    );
+  }
 }
