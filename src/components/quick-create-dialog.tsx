ead6b381551438052fc2d59f51a5764532fe1ba8
Sain-Biswas <sain.biswas614@gmail.com>
2025-10-12 23:48:05 +0530

    feat: create quick create dialog in the dashboard

--- src/components/quick-create-dialog.tsx
+++ src/components/quick-create-dialog.tsx
@@ -1,5 +1,30 @@
-import { Dialog } from "~/shadcn/ui/dialog";
+import { IconCirclePlusFilled } from "@tabler/icons-react";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from "~/shadcn/ui/dialog";
+import { SidebarMenuButton } from "~/shadcn/ui/sidebar";
 
 export function QuickCreateDialog() {
-  return <Dialog></Dialog>;
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <SidebarMenuButton
+          tooltip="Quick Create"
+          className="min-w-8 bg-primary text-primary-foreground duration-200 ease-linear hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground"
+        >
+          <IconCirclePlusFilled />
+          <span>Quick Create</span>
+        </SidebarMenuButton>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create New Link</DialogTitle>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
 }
--- src/shadcn/nav-main.tsx
+++ src/shadcn/nav-main.tsx
@@ -2,13 +2,13 @@
 
 import {
   IconChartBar,
-  IconCirclePlusFilled,
   IconLayout2Filled,
   IconLink,
   IconMail
 } from "@tabler/icons-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { QuickCreateDialog } from "~/components/quick-create-dialog";
 
 import { Button } from "~/shadcn/ui/button";
 import {
@@ -45,13 +45,7 @@ export function NavMain() {
       <SidebarGroupContent className="flex flex-col gap-2">
         <SidebarMenu>
           <SidebarMenuItem className="flex items-center gap-2">
-            <SidebarMenuButton
-              tooltip="Quick Create"
-              className="min-w-8 bg-primary text-primary-foreground duration-200 ease-linear hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground"
-            >
-              <IconCirclePlusFilled />
-              <span>Quick Create</span>
-            </SidebarMenuButton>
+            <QuickCreateDialog />
             <Button
               size="icon"
               className="size-8 group-data-[collapsible=icon]:opacity-0"
