c5217716f2a704af50736f43e77c2cab4eaf469e
Sain-Biswas <sain.biswas614@gmail.com>
2025-10-15 00:40:21 +0530

    feat: update url schema for defining active status

--- src/server/database/schema/url.schema.ts
+++ src/server/database/schema/url.schema.ts
@@ -1,5 +1,5 @@
 import { relations } from "drizzle-orm";
-import { json, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { boolean, json, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 import { folderSchema } from "./folders.schema";
 import { tagsSchema } from "./tags.schema";
 import { userSchema } from "./user.schema";
@@ -22,7 +22,8 @@ export const urlSchema = pgTable("url", {
     campaign: string;
     term: string;
     content: string;
-  }>()
+  }>(),
+  isActive: boolean("is_active").notNull().default(true)
 });
 
 export const urlToTagsSchema = pgTable("url_to_tags", {
